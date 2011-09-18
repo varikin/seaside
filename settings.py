1e3a11424f6cff6c4f080c3266ec18334172cadd
John Shimek <varikin@gmail.com>
2011-09-18 13:29:48 -0500

    Updated to Django 1.3.1.

--- settings.py
+++ settings.py
@@ -3,13 +3,30 @@
 import os
 import logging
 
-PROJECT_BASE = os.path.dirname(__file__)
+PROJECT_BASE = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     ('varikin', 'varikin@gmail.com'),
 )
 
 MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'dev.db',                      
+        'USER': '',     
+        'PASSWORD': '', 
+        'HOST': '',     
+        'PORT': '',
+    }
+}     
+
+SECRET_KEY = '*23ie0kskkq!=!ud59ke^8cxyu($f79wlx!lm!v=@l$&xy)45_'
+
 
 TIME_ZONE = 'America/Chicago'
 LANGUAGE_CODE = 'en-us'
@@ -18,17 +35,30 @@ USE_I18N = False
 
 MEDIA_ROOT = os.path.join(PROJECT_BASE, 'media')
 MEDIA_URL = '/media/'
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+STATIC_ROOT = os.path.join(PROJECT_BASE, 'static')
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+STATICFILES_DIR = (
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'seaside.urls'
@@ -43,12 +73,29 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'seaside.pages',
 )
 
-LOG_FILENAME = os.path.join(PROJECT_BASE, 'seaside.log')
-LOG_FORMAT = "%(asctime)s:%(levelname)s:%(module)s.%(funcName)s:%(message)s"
-logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG, format=LOG_FORMAT)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+   }
+}
 
 try:
     from local_settings import *
--- urls.py
+++ urls.py
@@ -1,24 +1,15 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
+from django.conf.urls.static import static
 from django.contrib import admin
 from seaside.pages.views import seaside
+
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
-)
-
-if settings.DEBUG:
-    import os.path
-
-    media = {'document_root': os.path.join(settings.PROJECT_BASE, 'media')}
-    static = {'document_root': os.path.join(settings.PROJECT_BASE, 'static')}
-    urlpatterns += patterns('django.views.static',
-        (r'^media/(?P<path>.*)$', 'serve', media),
-        (r'^static/(?P<path>.*)$', 'serve', static), 
-    )
-    
-urlpatterns += patterns('',
     (r'^(?P<url>.*)$', seaside),
-)
+) 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
